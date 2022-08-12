e070b3461dc70e085811d6f6d9698c9a28ca8435
DazKooL223 <nvkulkarni.1108@gmail.com>
2022-08-12 22:23:12 +0530

    added contract files

---
+++ contracts/fa2.py
@@ -0,0 +1,206 @@
+"""
+This is the customised FA2 contract having two functions
+1. mint():
+it verifies admin with few other conditions and mints tokens by updating 
+the ledger, supply mapping, counters and token metadata according the requirements of the lazyminting
+
+2. transfer():
+function for performing the transfer operation from the tokenOwner to the buyer.
+
+these both functions are called in the redeem() function in the lazymint contract
+"""
+
+import smartpy as sp
+
+FA2 = sp.io.import_template("FA2.py")
+
+
+class Types:
+    VOUCHER = sp.TRecord(
+        contractAddress=sp.TAddress,
+        tokenId=sp.TNat,
+        tokenOwner=sp.TAddress,
+        price=sp.TMutez,
+        tokenURI=sp.TString,
+    )
+
+    LEDGER_KEY = sp.TPair(
+        # tokenOwner
+        sp.TAddress,
+        # tokenId
+        sp.TNat,
+    )
+
+    TRANSFER = sp.TList(
+        sp.TRecord(
+            from_=sp.TAddress,
+            txs=sp.TList(sp.TRecord(to_=sp.TAddress, token_id=sp.TNat, amount=sp.TNat)),
+        )
+    )
+
+    OPERATOR_KEY = sp.TRecord(
+        # The owner of the token editions
+        owner=sp.TAddress,
+        # The operator allowed by the owner to transfer their token editions
+        operator=sp.TAddress,
+        # The token id
+        token_id=sp.TNat,
+    )
+
+
+class Token(sp.Contract):
+    def __init__(self, administrator):
+        # type setting of class
+        self.init_type(
+            sp.TRecord(
+                # address of admin
+                administrator=sp.TAddress,
+                # ledger where tokenOwners are listed
+                ledger=sp.TBigMap(Types.LEDGER_KEY, sp.TNat),
+                # counter that keeps track of total minted tokens
+                counter=sp.TNat,
+                # to keep track of total supply
+                supply=sp.TBigMap(sp.TNat, sp.TNat),
+                # stores the ipfs metadata link along with the token id
+                token_metadata=sp.TBigMap(sp.TNat, sp.TString),
+                # token_id field of fa2
+                token_id=sp.TNat,
+                # voucher which will be sent by the lazymint
+                operators=sp.TBigMap(Types.OPERATOR_KEY, sp.TUnit),
+            )
+        )
+        self.init(
+            administrator=administrator,
+            ledger=sp.big_map(),
+            counter=sp.nat(0),
+            supply=sp.big_map(),
+            token_metadata=sp.big_map(),
+            token_id=sp.nat(0),
+            operators=sp.big_map(),
+        )
+
+    def check_is_administrator(self):
+        sp.verify(sp.sender == self.data.administrator, message="FA2_NOT_ADMIN")
+
+    def check_token_exists(self, token_id):
+        sp.verify(token_id < self.data.counter, message="FA2_TOKEN_UNDEFINED")
+
+    @sp.entry_point
+    def mint(self, params):
+        sp.set_type(params, Types.VOUCHER)
+        self.check_is_administrator()
+
+        # updating the values in storage
+
+        token_id = sp.compute(self.data.counter)
+
+        self.data.ledger[(params.tokenOwner, token_id)] = sp.utils.mutez_to_nat(
+            params.price
+        )
+
+        self.data.supply[token_id] = sp.utils.mutez_to_nat(params.price)
+
+        self.data.token_metadata[token_id] = params.tokenURI
+
+        self.data.counter += 1
+
+    @sp.entry_point
+    def transfer(self, params):
+        sp.set_type(params, Types.TRANSFER)
+
+        with sp.for_("transfer", params) as transfer:
+            with sp.for_("tx", transfer.txs) as tx:
+                # Check that the token exists
+                token_id = sp.compute(tx.token_id)
+                self.check_token_exists(token_id)
+
+                # Check that the sender is one of the token operators
+                owner = sp.compute(transfer.from_)
+                sp.verify(
+                    (sp.sender == owner)
+                    | self.data.operators.contains(
+                        sp.record(owner=owner, operator=sp.sender, token_id=token_id)
+                    ),
+                    message="FA2 OPERATOR ERROR",
+                )
+
+                # Check that the transfer amount is not zero
+                with sp.if_(tx.amount > 0):
+                    # Remove the token amount from the owner
+                    owner_key = sp.pair(owner, token_id)
+                    self.data.ledger[owner_key] = sp.as_nat(
+                        self.data.ledger.get(owner_key, 0) - tx.amount,
+                        "FA2_INSUFFICIENT_BALANCE",
+                    )
+
+                    # Add the token amount to the new owner
+                    new_owner_key = sp.pair(tx.to_, token_id)
+                    self.data.ledger[new_owner_key] = (
+                        self.data.ledger.get(new_owner_key, 0) + tx.amount
+                    )
+
+
+@sp.add_test(name="mint")
+def test():
+    scenario = sp.test_scenario()
+    # sample accounts
+    admin = sp.test_account("admin")
+    # admin address = tz1UyQDepgtUBnWjyzzonqeDwaiWoQzRKSP5
+    musk = sp.test_account("user1")
+    elon = sp.test_account("user2")
+    # elon address = tz1KouJrf2JFUx8y15pZv7B36dtYQ3GcgJu3
+    # instace of token class
+    token = Token(administrator=admin.address)
+    scenario += token
+    sampleToken0 = sp.record(
+        contractAddress=sp.address("KT1"),
+        tokenId=sp.nat(0),
+        tokenOwner=admin.address,
+        price=sp.mutez(69),
+        tokenURI=sp.string("ipfs://icandowhateveriwant"),
+    )
+    # token minted by admin
+    scenario += token.mint(sampleToken0).run(sender=admin)
+
+    sampleToken1 = sp.record(
+        contractAddress=sp.address("KT1"),
+        tokenId=sp.nat(0),
+        tokenOwner=admin.address,
+        price=sp.mutez(69),
+        tokenURI=sp.string("ipfs://icantdoanythingevenifiamelon"),
+    )
+    # token minted by elon (not admin)
+    scenario += token.mint(sampleToken1).run(sender=elon, valid=False)
+
+    # sending invalid token
+    tokentobetransfered1 = [
+        sp.record(
+            from_=admin.address,
+            txs=[sp.record(to_=elon.address, token_id=69, amount=69)],
+        )
+    ]
+    scenario += token.transfer(tokentobetransfered1).run(sender=admin, valid=False)
+
+    # sending token with incorrect owner
+    tokentobetransfered2 = [
+        sp.record(
+            from_=musk.address, txs=[sp.record(to_=elon.address, token_id=0, amount=69)]
+        )
+    ]
+    scenario += token.transfer(tokentobetransfered2).run(sender=admin, valid=False)
+
+    # sending invalid amount
+    tokentobetransfered3 = [
+        sp.record(
+            from_=admin.address, txs=[sp.record(to_=elon.address, token_id=0, amount=1)]
+        )
+    ]
+    scenario += token.transfer(tokentobetransfered3).run(sender=admin)
+
+    # admin transfers token to elon without any error
+    tt = [
+        sp.record(
+            from_=admin.address, txs=[sp.record(to_=elon.address, token_id=0, amount=1)]
+        )
+    ]
+    scenario += token.transfer(tt).run(sender=admin)
---
+++ contracts/lazymint.py
@@ -0,0 +1,224 @@
+"""
+This is the Lazymint contract in with redeem() function
+
+A 'voucher' with suitable parameters (i.e. fields required for minting, signature, publickey of the owner)
+are sent to the redeem() along with the redeemers address
+then the function :
+1. verifies the signature that the message is actually signed by the tokenOwner.
+2. mints the token with appropriate data fields with the fa2.mint() on the address of the tokenOwner.
+3. transfers the token from the tokenOwner to redeemer with the fa2.mint().
+4. invalidates the voucher so that it is not used again.
+
+this function is called by the redeemer.
+"""
+import smartpy as sp
+
+# class for type setting of records
+class Types:
+    MESSAGE = sp.TRecord(
+        contractAddress=sp.TAddress,
+        tokenId=sp.TNat,
+        tokenOwner=sp.TAddress,
+        price=sp.TMutez,
+        tokenURI=sp.TString,
+    )
+    VOUCHER = sp.TRecord(
+        message=MESSAGE,
+        signature=sp.TSignature,
+        publickey=sp.TKey,
+    )
+    TRANSFER = sp.TRecord(
+        from_=sp.TAddress,
+        txs=sp.TRecord(to_=sp.TAddress, token_id=sp.TNat, amount=sp.TNat),
+    )
+
+
+# class for performing lazyminting operations
+
+
+class LazyMint(sp.Contract):
+    def __init__(
+        self,
+        admin,
+        contractAddress,
+        counter=sp.nat(0),
+        invalidated_voucher=sp.big_map(),
+    ):
+
+        self.init_type(
+            sp.TRecord(
+                # admin for initialise the contract
+                admin=sp.TAddress,
+                # bigmap to maintain record of minted tokens
+                counter=sp.TNat,
+                contractAddress=sp.TAddress,
+                invalidated_voucher=sp.TBigMap(sp.TBytes, sp.TBool),
+            )
+        )
+
+        # initialise the storage
+        self.init(
+            admin=admin,
+            contractAddress=sp.address("KT1LPSGeRj4FENhm9anHJUQy9epAwtttDwLh"),
+            counter=0,
+            invalidated_voucher=sp.big_map(),
+        )
+
+    # signature verification function
+    def verify_signature(self, voucher):
+        sp.set_type(voucher, Types.VOUCHER)
+
+        # creates a replica digest in the format of the request sent from the frontend
+        digest = sp.pack(voucher.message)
+
+        # uses the check_signature with the tokenOwner and the digest
+        isValid = sp.check_signature(voucher.publickey, voucher.signature, digest)
+        sp.verify(isValid, "INVALID SIGNATURE")
+
+    @sp.entry_point
+    def redeem(self, redeemer, voucher):
+        # type setting of each parameter
+        sp.set_type(redeemer, sp.TAddress)
+        sp.set_type(voucher, Types.VOUCHER)
+
+        message = voucher.message
+        # verify the signature with the digest and the signature
+        self.verify_signature(voucher)
+        # condition to check the if redeem is called by the redeemer
+        sp.verify(sp.sender == redeemer, "INVALID SENDER")
+        # check whether buyer has sent enough tez to buy the nft
+        sp.verify(sp.amount == message.price, "INVALID AMOUNT")
+        # check the token is already minted or not
+        sp.verify(voucher.message.tokenId > self.data.counter, "TOKEN ALREADY MINTED")
+        # check the voucher is valid
+        digest = sp.pack(voucher.message)
+        sp.verify(~self.data.invalidated_voucher.contains(digest), "INVALID VOUCHER")
+        # mint the token to the tokenOwner using the data present in the voucher
+        fa2mint = sp.contract(
+            Types.MESSAGE, self.data.contractAddress, entry_point="mint"
+        ).open_some()
+
+        sp.transfer(
+            arg=sp.record(
+                contractAddress=message.contractAddress,
+                tokenId=message.tokenId,
+                tokenOwner=message.tokenOwner,
+                price=message.price,
+                tokenURI=message.tokenURI,
+            ),
+            amount=sp.mutez(0),
+            destination=fa2mint,
+        )
+        # increase the counter
+        self.data.counter += 1
+        # transfer the tokens from the tokenOwner to the redeemer
+        fa2transfer = sp.contract(
+            Types.TRANSFER,
+            address=self.data.contractAddress,
+            entry_point="transfer",
+        ).open_some()
+
+        sp.transfer(
+            arg=sp.record(
+                from_=message.tokenOwner,
+                txs=sp.record(to_=redeemer, token_id=message.tokenId, amount=sp.nat(1)),
+            ),
+            amount=sp.mutez(0),
+            destination=fa2transfer,
+        )
+        # sending tez to the tokenOwner
+        sp.send(message.tokenOwner, sp.amount, message="COULD NOT SEND")
+        # Invalidate the voucher to further prevent it to mint on the other marketplaces
+        digest = sp.pack(voucher.message)
+        self.data.invalidated_voucher[digest] = sp.bool(True)
+
+
+@sp.add_test(name="lazymint works fine trust me")
+def test():
+    scenario = sp.test_scenario()
+    # test accounts
+    admin = sp.test_account("admin")
+    elon = sp.test_account("elon")
+    pikachu = sp.test_account("pikachu")
+
+    lazymint = LazyMint(
+        admin=admin.address,
+        contractAddress=sp.address("KT1LPSGeRj4FENhm9anHJUQy9epAwtttDwLh"),
+    )
+    scenario += lazymint
+    message = sp.record(
+        contractAddress=sp.address("KT1LPSGeRj4FENhm9anHJUQy9epAwtttDwLh"),
+        tokenId=1,
+        tokenOwner=admin.address,
+        price=sp.mutez(69),
+        tokenURI=sp.string("ipfs://dogetothemoon"),
+    )
+    digest = sp.pack(message)
+    voucher = sp.record(
+        message=message,
+        signature=sp.make_signature(admin.secret_key, digest, message_format="Raw"),
+        publickey=admin.public_key,
+    )
+    scenario += lazymint.redeem(redeemer=elon.address, voucher=voucher).run(
+        sender=elon.address, amount=message.price
+    )
+    scenario.show(lazymint.balance)
+    # validates the voucher is invalidated
+    scenario.verify(lazymint.data.invalidated_voucher[digest] == sp.bool(True))
+    ## invalid tests
+    # token already minted error
+    scenario += lazymint.redeem(redeemer=elon.address, voucher=voucher).run(
+        sender=elon.address, amount=message.price, valid=False
+    )
+
+    # invalid amount error
+    message = sp.record(
+        contractAddress=sp.address("KT1LPSGeRj4FENhm9anHJUQy9epAwtttDwLh"),
+        tokenId=2,
+        tokenOwner=admin.address,
+        price=sp.mutez(69),
+        tokenURI=sp.string("ipfs://abkibaarmodisarkar"),
+    )
+    digest = sp.pack(message)
+    voucher = sp.record(
+        message=message,
+        signature=sp.make_signature(admin.secret_key, digest, message_format="Raw"),
+        publickey=admin.public_key,
+    )
+    scenario += lazymint.redeem(redeemer=elon.address, voucher=voucher).run(
+        sender=elon.address, amount=sp.mutez(420), valid=False
+    )
+    # invalid signature error
+    message = sp.record(
+        contractAddress=sp.address("KT1LPSGeRj4FENhm9anHJUQy9epAwtttDwLh"),
+        tokenId=3,
+        tokenOwner=admin.address,
+        price=sp.mutez(69),
+        tokenURI=sp.string("ipfs://tezbetterthaneth"),
+    )
+    digest = sp.pack(message)
+    voucher = sp.record(
+        message=message,
+        signature=sp.make_signature(admin.secret_key, digest, message_format="Raw"),
+        publickey=elon.public_key,
+    )
+    scenario += lazymint.redeem(redeemer=elon.address, voucher=voucher).run(
+        sender=elon.address, amount=sp.mutez(420), valid=False
+    )
+    # invalid sender
+    message = sp.record(
+        contractAddress=sp.address("KT1LPSGeRj4FENhm9anHJUQy9epAwtttDwLh"),
+        tokenId=4,
+        tokenOwner=admin.address,
+        price=sp.mutez(69),
+        tokenURI=sp.string("ipfs://iamdonebeingfunny"),
+    )
+    digest = sp.pack(message)
+    voucher = sp.record(
+        message=message,
+        signature=sp.make_signature(admin.secret_key, digest, message_format="Raw"),
+        publickey=admin.public_key,
+    )
+    scenario += lazymint.redeem(redeemer=elon.address, voucher=voucher).run(
+        sender=pikachu.address, amount=sp.mutez(420), valid=False
+    )
